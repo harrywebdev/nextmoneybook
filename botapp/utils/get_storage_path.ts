cfe00273dd1befe74d97d0d9fee26eebea19b0b2
Harry B <harry@gethornet.com>
2023-04-17 18:36:27 +0200

    Fix storage path for mounted folder

--- botapp/utils/get_storage_path.ts
+++ botapp/utils/get_storage_path.ts
@@ -1,7 +1,12 @@
 import path from "path";
 
 export function getStoragePath(appendPath: string = "") {
-  let storagePath = process.env.STORAGE_PATH || "/data";
+  // if STORAGE_PATH is not set, use /data from the root
+  if (!process.env.STORAGE_PATH) {
+    return path.join('/data', appendPath);
+  }
+  
+  let storagePath = process.env.STORAGE_PATH;
   storagePath = storagePath[0] === "/" ? storagePath : `/${storagePath}`;
 
   return path.join(__dirname, `../..${storagePath}/`, appendPath);
